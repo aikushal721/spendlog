a31f541fbcb13e2f146543956a923c6fb3bb57b1
aikushal721 <ai.kushal721@gmail.com>
2026-03-20 21:35:40 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='spendlog-v1';
+const ASSETS=['/spendlog/','/spendlog/index.html','/spendlog/manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
